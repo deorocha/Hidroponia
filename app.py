3bfdbfcc4738fab92193c2a39f3aed3a4076f561
André Rocha <106490437+deorocha@users.noreply.github.com>
2025-06-10 18:03:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,16 @@ PAGES = {
     "tabelas": "📋 Tabelas"
 }
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            div.embeddedAppMetaInfoBar_container__DxxL1 {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # CSS personalizado
 st.markdown(
     """
