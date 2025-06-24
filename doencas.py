c645a44a8ef55bf28cf664dadf169e4b936cf108
André Rocha <106490437+deorocha@users.noreply.github.com>
2025-06-24 10:23:51 -0300

    Update doencas.py

--- doencas.py
+++ doencas.py
@@ -4,11 +4,16 @@ import streamlit as st
 import numpy as np
 import os
 from PIL import Image
-from keras.models import load_model, save_model
-from keras.applications.mobilenet_v2 import preprocess_input
-from keras.models import Sequential
-from keras.layers import InputLayer, Flatten, Dense
-from keras.optimizers import Adam
+#from keras.models import load_model, save_model
+#from keras.applications.mobilenet_v2 import preprocess_input
+#from keras.models import Sequential
+#from keras.layers import InputLayer, Flatten, Dense
+#from keras.optimizers import Adam
+# Atualize estas importações:
+from tensorflow.keras.models import load_model, save_model, Sequential
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.layers import InputLayer, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
 
 model_path = "modelos/keras_model.h5"
 classes = open("modelos/labels.txt", "r").readlines()
