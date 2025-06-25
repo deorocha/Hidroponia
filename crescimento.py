ce91245b9b4f32e7612f40f4231b10f753b818ed
André Rocha <106490437+deorocha@users.noreply.github.com>
2025-06-25 20:46:33 -0300

    Add files via upload

--- crescimento.py
+++ crescimento.py
@@ -1,4 +1,4 @@
-# crescimento.py
+# produtividade.py
 
 import streamlit as st
 
@@ -21,16 +21,26 @@ def main():
             padding-left: 0.5rem;
             padding-right: 0.5rem;
         }}
+        
+        .construction {{
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 5rem !important;
+            text-align: center;
+        }}
         </style>
     """, unsafe_allow_html=True)
 
-    # Simulação de interação
-    user_input = st.text_input("Digite algo:")
-    if user_input:
-        st.write(f"📋: Você disse: {user_input}")
+    # Mensagem centralizada com estilo personalizado
+    st.markdown(
+        '<div class="construction">🚧 Em Construção.</div>', 
+        unsafe_allow_html=True
+    )
 
     # Botão para voltar ao menu principal
-#    st.markdown('<a href="/" target="_self"><button style="margin-top:20px;">Voltar ao Menu Principal</button></a>', unsafe_allow_html=True)
+    st.markdown('<a href="/" target="_self"><button style="margin-top:20px;">Voltar ao Menu Principal</button></a>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
